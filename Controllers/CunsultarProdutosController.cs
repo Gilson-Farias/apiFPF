5b94e3239956e266b1fdb9682eda75abdc3c4475
Gilson Farias <gilsonfarias@hotmail.com>
2021-06-13 19:49:36 -0400

    Comit 001

---
+++ Controllers/CunsultarProdutosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace apiFPF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConsultarProdutoController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ConsultarProdutoController> _logger;
+        public ConsultarProdutoController(ILogger<ConsultarProdutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<produto>), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult Get(int codigo)
+        {
+            try
+            {
+                return Ok(new Business.produtoBusiness().GetAllConsultaProd(codigo));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Controllers/IncluirProdutoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace apiFPF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IncluirProdutoController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<IncluirProdutoController> _logger;
+        public IncluirProdutoController(ILogger<IncluirProdutoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<produto>), 200)]
+        [ProducesResponseType(500)]
+        public IActionResult Get(int codigo, string nome, decimal quantidade, decimal valor)
+        {
+            var produto = new produto();
+
+            produto.codigo = codigo;
+            produto.nome = nome;
+            produto.quantidade = quantidade;
+            produto.valorUnitario = valor;
+
+            try
+            {
+                return Ok(new Business.produtoBusiness().GetAllIncluirProd(produto));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
